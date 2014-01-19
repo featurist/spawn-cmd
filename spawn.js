26a41cc57098bb0e81ca4113f3eda8421800e83b
Josh Chisholm <joshuachisholm@gmail.com>
2014-01-19 13:15:53 +0000

    port spawn.js fixes back to spawn.pogo

--- spawn.js
+++ spawn.js
@@ -3,9 +3,9 @@
     var spawn, windowsSpawn;
     spawn = require("child_process").spawn;
     windowsSpawn = function(executable, args, options) {
-        return spawn(process.env.comspec || 'cmd.exe', [ "/c", executable ].concat(args), options);
+        return spawn(process.env.comspec || "cmd.exe", [ "/c", executable ].concat(args), options);
     };
-    if (process.platform === 'win32') {
+    if (process.platform === "win32") {
         exports.spawn = windowsSpawn;
     } else {
         exports.spawn = spawn;
